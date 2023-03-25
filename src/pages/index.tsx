19d6b19922488da039366da821f5036706dde7cf
Miguel Hernández Gaitan <miguelhernandezgaitan13@gmail.com>
2023-03-25 08:29:30 -0600

    router

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -6,8 +6,8 @@ import Link from "next/link";
 import { api } from "~/utils/api";
 
 const Home: NextPage = () => {
-  const hello = api.example.hello.useQuery({ text: "from tRPC" });
-  const user = useUser()
+  const user = useUser();
+  const { data } = api.post.getAll.useQuery();
 
   return (
     <>
@@ -18,11 +18,14 @@ const Home: NextPage = () => {
       </Head>
       <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c]">
         <div>
-        { ! user.isSignedIn && <SignInButton /> }
-        { !!user.isSignedIn && <SignOutButton /> }
-
+          {!user.isSignedIn && <SignInButton />}
+          {!!user.isSignedIn && <SignOutButton />}
         </div>
-          
+        <div>
+          {data?.map((post) => (
+            <div key={post.id}>{post.content}</div>
+          ))}
+        </div>
       </main>
     </>
   );
