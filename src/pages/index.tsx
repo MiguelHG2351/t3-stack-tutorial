a6a9b50191b802402a455cc407a387805f7df7ab
Miguel Hernández Gaitan <miguelhernandezgaitan13@gmail.com>
2023-03-28 09:24:06 -0600

    font sizing

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -57,7 +57,7 @@ const PostView = (props: PostViewWithUser) => {
             post.createdAt
           ).fromNow()}`}</span>
         </div>
-        <span>{post.content}</span>
+        <span className="text-2xl">{post.content}</span>
       </div>
     </div>
   );
