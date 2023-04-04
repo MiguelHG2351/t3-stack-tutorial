a734d1145b4cfaec286cdd4a99523e37303b6d6b
Miguel Hernández Gaitan <miguelhernandezgaitan13@gmail.com>
2023-04-04 11:06:23 -0600

    Profile page with ssg helper

--- src/server/api/routers/post.ts
+++ src/server/api/routers/post.ts
@@ -1,21 +1,17 @@
-import type { User } from "@clerk/nextjs/dist/api";
 import { clerkClient } from "@clerk/nextjs/server";
 import { TRPCError } from "@trpc/server";
 import { z } from "zod";
 // import { z } from "zod";
 
-import { createTRPCRouter, privateProcedure, publicProcedure } from "~/server/api/trpc";
-
-const filterUserForClient = (user: User) => {
-  return {
-    id: user.id,
-    username: user.username,
-    profileImageUrl: user.profileImageUrl,
-  };
-};
+import {
+  createTRPCRouter,
+  privateProcedure,
+  publicProcedure,
+} from "~/server/api/trpc";
 
 import { Ratelimit } from "@upstash/ratelimit"; // for deno: see above
 import { Redis } from "@upstash/redis";
+import { filterUserForClient } from "~/server/helper/filterUserForClient";
 
 // Create a new ratelimiter, that allows 3 requests per 1 minute
 const ratelimit = new Ratelimit({
@@ -26,7 +22,7 @@ const ratelimit = new Ratelimit({
    * Optional prefix for the keys used in redis. This is useful if you want to share a redis
    * instance with other applications and want to avoid key collisions. The default prefix is
    * "@upstash/ratelimit"
-   */ 
+   */
   prefix: "@upstash/ratelimit",
 });
 
@@ -36,7 +32,7 @@ export const postRouter = createTRPCRouter({
       take: 100,
       orderBy: {
         createdAt: "desc",
-      }
+      },
     });
 
     const users = (
@@ -51,7 +47,7 @@ export const postRouter = createTRPCRouter({
     return posts.map((post) => {
       const author = users.find((user) => user.id === post.authorId);
 
-      if(!author || !author.username) {
+      if (!author || !author.username) {
         throw new TRPCError({
           code: "INTERNAL_SERVER_ERROR",
           message: "Could not find author for post",
@@ -67,24 +63,29 @@ export const postRouter = createTRPCRouter({
       };
     });
   }),
-  create: privateProcedure.input(z.object({
-    content: z.string().emoji('Only emojis are allowed').min(1).max(280),
-  })).mutation(async ({ ctx, input }) => {
-    const authorId = ctx.userId;
+  create: privateProcedure
+    .input(
+      z.object({
+        content: z.string().emoji("Only emojis are allowed").min(1).max(280),
+      })
+    )
+    .mutation(async ({ ctx, input }) => {
+      const authorId = ctx.userId;
 
-    const { success } = await ratelimit.limit(authorId);
+      const { success } = await ratelimit.limit(authorId);
 
-    if(!success) throw new TRPCError({
-      code: "TOO_MANY_REQUESTS",
-    }) 
+      if (!success)
+        throw new TRPCError({
+          code: "TOO_MANY_REQUESTS",
+        });
 
-    const post = await ctx.prisma.post.create({
-      data: {
-        authorId, 
-        content: input.content,
-      }
-    })
+      const post = await ctx.prisma.post.create({
+        data: {
+          authorId,
+          content: input.content,
+        },
+      });
 
-    return post;
-  })
+      return post;
+    }),
 });
